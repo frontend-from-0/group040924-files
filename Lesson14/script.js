40502be0db69e04559a87790556304dc12184cf1
Anna S. A <anna.atalay@minna.tech>
2024-12-18 20:08:02 +0100

    Add Lesson14 completed files

--- Lesson14/script.js
+++ Lesson14/script.js
@@ -4,39 +4,95 @@
 
 // 1. Declare variables firstNumber=5 and secondNumber=3 and log their sum.
 // CODE HERE
+const firstNumber = 5;
+const secondNumber = 3;
+
+const sumEx1 = firstNumber + secondNumber;
+
+console.log(sumEx1);
 
 // 2. Declare variables userName and userAge. Log a greeting: "Hello! I am (userName) and I am (userAge) years old."
 // CODE HERE
+const userName = "Baljan";
+const userAge = 28;
+
+console.log(`Hello! I am ${userName} and I am ${userAge} years old.`);
+console.log("Hello! I am " + userName + " and I am " + userAge + " years old.");
 
 // 3. Declare variables a=10 and b=4. Log the result of a-b, a*b, and a/b.
 // CODE HERE
+const a = 10;
+const b = 4;
+
+const toplam = a + b;
+const carpim = a * b;
+const bolum = a / b;
+console.log("Toplam sonucu= " + toplam);
+// 1. Create a single string from "Toplam sonucu= " + toplam
+// 2. Log the result using console.log()
+console.log("Çarpım sonucu= " + carpim);
+console.log("Bölüm sonucu= " + bolum);
 
 // 4. Use template literals to log: "My name is (userName). I like JS."
 // CODE HERE
+console.log(`My name is ${userName}. I like JS.`);
 
 // 5. Declare a string password = "securePass". Log the length of password.
 // CODE HERE
+const password = "securePass";
+console.log(password.length);
 
 // 6. Convert the string "hello world" to uppercase and log it.
 // CODE HERE
+const word = "hello world";
+console.log(word.toUpperCase());
+console.log("hello world".toUpperCase());
 
 // 7. Concatenate "Hello" and "World" with a space in between and log the result.
 // CODE HERE
 
 // 8. Check the type of a variable, e.g., let x = 42. Log the type using typeof.
 // CODE HERE
+const name = "sevcan";
+const city = "mersin";
+
+console.log(typeof name, typeof city, typeof userAge);
 
 // 9. Convert the number 100 to a string and log the result.
 // CODE HERE
+const number = 100;
+const numberString = number.toString();
+console.log(number, numberString);
+// This will create a string from a number and a string!!! console.log(number + numberString);
 
 // 10. Convert the string "50" to a number and log its type to confirm the conversion.
 // CODE HERE
+const string = "50";
+
+console.log(parseInt(string));
 
 // 11. Generate a random integer between 0 and 10 and log it.
 // CODE HERE
+const randomNumber = Math.random() * 11; // 0 -> 10.99999
+console.log('Random number ex. 11', Math.floor(randomNumber));
 
 // 12. Round the number 3.7 down using Math.floor and 3.2 up using Math.ceil, log both.
 // CODE HERE
+const number1Ex12 = Math.floor(3.7);
+console.log(number1Ex12);
+const number2Ex12 = Math.ceil(3.2);
+console.log(number2Ex12);
+
+
+// Prompt can only be used in browser environment
+const userResponse = prompt("What is your age?");
+const userResposeNumber = parseInt(userResponse);
+
+if (isNaN(userResposeNumber)) {
+  throw new Error("Please enter a valid number");
+} else {
+  console.log('userResponse',  Math.floor(userResposeNumber));
+}
 
 // 13. Declare a boolean variable isStudent = true. Log it.
 // CODE HERE
