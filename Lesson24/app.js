b46e23e4cbb8bb8afa1bbaf51d3ff6c71148b4dc
Anna S. A <anna.atalay@minna.tech>
2025-03-12 21:18:02 +0100

    Add Lesson24 completed files

--- Lesson24/app.js
+++ Lesson24/app.js
@@ -4,6 +4,8 @@ C - create - POST method (has request body to transfer data)
 R - read - GET method (cannot have request body to send data to the server)
 U - update - PUT / PATCH method (have request body to transfer data)
 D - delete - DELETE method
+
+
 Status codes
 HTTP status codes are three-digit numbers that the server sends in response to a client's request made to a web server. They provide information about the outcome of the request, whether it was successful, encountered an error, or requires further action. HTTP status codes are grouped into several ranges, each indicating a different category of response. 
 100... - Informational Responses
@@ -13,20 +15,52 @@ HTTP status codes are three-digit numbers that the server sends in response to a
 500... - Service error (500 Internal Server Error, 502 Bad Gateway, 503 Service Unavailable)
 */
 
-const URL = 'https://jsonplaceholder.typicode.com/posts';
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+document.getElementById("fetch-posts").addEventListener("click", getPosts);
 
 function getPosts() {
+  console.log("Getting posts");
+  fetch(URL)
+    .then((response) => response.json())
+    .then((posts) => {
+      posts.forEach((post) => {
+        const liItem = document.createElement("li");
+        liItem.classList.add("post");
+        const postTitle = document.createElement("h2");
+        postTitle.classList.add("post-title");
+        postTitle.textContent = posts.title;
+        const pItem = document.createElement("p");
+        pItem.classList.add("post-body");
+        pItem.textContent = post.body;
+
+        const updatePostButton = document.createElement("a");
+        updatePostButton.href = `./update-post.html?id=${post.id}`;
+        updatePostButton.textContent = "Update";
+        updatePostButton.classList.add("button", "button--success");
+
+        const deletePostButton = document.createElement("button");
+        deletePostButton.textContent = "Delete";
+        deletePostButton.addEventListener("click", () => deletePost(post.id));
+        deletePostButton.classList.add("button", "button--danger");
+
+        liItem.appendChild(postTitle);
+        liItem.appendChild(pItem);
+        liItem.appendChild(updatePostButton);
+        liItem.appendChild(deletePostButton);
+        document.getElementById("posts-container").appendChild(liItem);
+      });
+    });
 }
 
-function getPostById() {
+function getPostById() {}
 
-}
+function createPost() {}
 
-function createPost() {
-}
+function updatePost() {}
 
-function updatePost() {
-}
-
-function deletePost() {
+function deletePost(postId) {
+  fetch(`${URL}/${postId}`, {
+    method: "DELETE",
+  });
 }
