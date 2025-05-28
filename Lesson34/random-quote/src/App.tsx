e8bd3d65a106a68863bd56c3c897b4d9dd42bb83
code2career <info@annaatalay.com>
2025-05-28 19:03:36 +0200

    Add Lesson34 starter files

---
+++ Lesson34/random-quote/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { ProfilePage } from './pages/ProfilePage';
+import { MainPage } from './pages/MainPage';
+import {PostsPage} from './pages/PostsPage'
+
+enum Page {
+  home = 'Home',
+  posts = 'Posts',
+  profile = 'Profile',
+  settings = 'Settings',
+}
+
+const allPages = Object.values(Page);
+
+function App() {
+  const [currentPage, setCurrentPage] = useState<Page>(Page.home);
+
+  return (
+    <div className='bg-white dark:bg-slate-800 min-h-screen'>
+      <nav className='bg-slate-400 text-slate-900'>
+        <ul className='flex gap-5 justify-end max-w-5xl'>
+          {allPages.map((page) => (
+            <li>
+              <button
+                className={'py-2.5 px-3'}
+                onClick={() => setCurrentPage(page)}
+              >
+                {page}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      {currentPage === Page.posts && <PostsPage />}
+      {currentPage === Page.home && <MainPage />}
+      {currentPage === Page.profile && <ProfilePage />}
+      {currentPage === Page.settings && <p>Settings</p>}
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson34/random-quote/src/pages/MainPage/index.jsx
@@ -0,0 +1,29 @@
+import { QuoteCard } from "../../components/QuoteCard";
+import { Button } from "../../components/Button";
+import { useQuotesContext } from "../../QuotesContextProvider";
+import {
+  useQuoteIndexContext,
+  useQuoteIndexDispatchContext,
+} from "../../QuoteIndexContextProvider";
+
+export const MainPage = () => {
+  const quotes = useQuotesContext();
+  const currentIndex = useQuoteIndexContext();
+  const dispatchQuoteIndex = useQuoteIndexDispatchContext();
+
+  function handleNextQuoteClick() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    dispatchQuoteIndex(randomIndex);
+  }
+  return (
+    <main>
+      <QuoteCard
+        quote={quotes[currentIndex].quote}
+        author={quotes[currentIndex].author}
+        likedBy={quotes[currentIndex].likedBy}
+      />
+
+      <Button label="Next quote" handleOnClick={handleNextQuoteClick} />
+    </main>
+  );
+};
