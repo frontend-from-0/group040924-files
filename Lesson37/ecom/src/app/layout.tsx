e174779f7d8f3f636a894332801136dffb1b7d3e
code2career <info@annaatalay.com>
2025-06-25 21:40:17 +0200

    Add Lesson37 completed files

--- Lesson37/ecom/src/app/layout.tsx
+++ Lesson37/ecom/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <ErrorBoundary fallback={undefined}>  {children} */}
         {children}
-        {/* </ErrorBoundary> */}
       </body>
     </html>
   );
